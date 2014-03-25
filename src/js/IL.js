979a35ff75f215db5e3cd049aa2472e30bdb105a
Pablo Garcia <pgarciacamou@gmail.com>
2014-03-25 14:09:37 -0700

    Adding images, calculating place wrong

--- src/js/IL.js
+++ src/js/IL.js
@@ -1,21 +1,24 @@
-(function($){
-	if(!$) throw "You need jQuery to run IL";
+var IL = (function($){
+	if(!$) return "You need jQuery to run IL";
+	var imagesElements = {}, imagesObjects = {};
 
 	function appendToCol(element, imagesElementID){
+		console.log(arguments);
 		var imagesElement = imagesElements[imagesElementID],
-		arr = $(imagesElementID).children(imagesElement.cols._identifier);
-		arr.sort(function(a, b){
+		arr = $(imagesElementID).children("."+imagesElement.cols._identifier);
+		arr = arr.sort(function(a, b){
 			var aHeight = Number($(a).css("height").replace("px","")), bHeight = Number($(b).css("height").replace("px",""));
 			return ((aHeight < bHeight) ? -1 : ((aHeight > bHeight) ? 1 : 0));
 		});
 		$(arr[0]).append(element);
 	}
-	function getObject(imagesElementID, parent){
-		return imagesObjects[imagesElementID] || imagesObjects[imagesElementID] = {
+	function getObject(imagesElementID){
+		var parent = imagesElements[imagesElementID];
+		return imagesObjects[imagesElementID] || (imagesObjects[imagesElementID] = {
 			set: function (options){
-				if(parent.cols._identifier != "col-sm-") throw "You can't set a image element more than once";
+				if(parent.cols._identifier != "col-sm-") return "You can't set a image element more than once";
 				parent.cols._num = options.cols || 4;
-				if(parent.cols._num < 0 || parent.cols._num > 12 || 12%cols_counter != 0) throw "Number of columns must be positive evenly divisor of 12";
+				if(parent.cols._num < 0 || parent.cols._num > 12 || 12 % parent.cols._num != 0) return "Number of columns must be positive evenly divisor of 12";
 				parent.cols._length = 12 / parent.cols._num;
 				parent.cols._identifier += parent.cols._length;
 				var index;
@@ -27,8 +30,8 @@
 				}
 			},
 			add: function (options){
-				if(!options || !options.imgSrc) throw "To add an image, you must specify options with at least imgSrc";
-				var imgSrc = options.imgSrc, imgDesc = options.imgDesc, linkBoolean = options.linkBoolean, 
+				if(!options || !options.imgSrc) return "To add an image, you must specify options with at least imgSrc";
+				var imgSrc = options.imgSrc, imgDesc = options.imgDesc, linkBoolean = options.link, 
 				imgAlt = options.imgAlt, imgTitle = options.imgTitle, imgHoverCSS = options.imgHoverCSS,
 				link, image;
 				if(linkBoolean){
@@ -48,7 +51,7 @@
 
 				var row = document.createElement("div"), col = document.createElement("div");
 				$(row).addClass("row");
-				$(col).addClass(parent.cols._identifier);
+				$(col).addClass("col-sm-12");
 				$(col).append(link);
 				$(row).append(col);
 
@@ -57,20 +60,18 @@
 			addElement: function (el){
 
 			}
-		};
+		});
 	}
-	Object.defineProperty(window, "IL", {
-		value: function (imagesElementID) {
-			if(!document.getElementById(imagesElementID)) throw "Id not recognized within the DOM tree";
-			var parent = imagesElements[imagesElementID] = imagesElements[imagesElementID] || ($(imagesElementID).addClass("row"), {
-				cols:{
-					_num:4,
-					_identifier:"col-sm-",
-					_length:3
-				}
-			});
-			return getObject(imagesElementID, parent);
+	return  function (imagesElementID) {
+		if(!document.getElementById(imagesElementID.slice(1))) return "Id not recognized within the DOM tree";
+		imagesElements[imagesElementID] = imagesElements[imagesElementID] || ($(imagesElementID).addClass("row"), {
+			cols:{
+				_num:4,
+				_identifier:"col-sm-",
+				_length:3
+			}
+		});
+		return getObject(imagesElementID);
+	};
 
-		}
-	});
 })(jQuery);
